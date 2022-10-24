a0c5e452980b443b475c3a338b9f239efdca6e98
yenssyleon <ykleonm@correo.udistrital.edu.co>
2022-10-24 16:34:41 -0500

    COMMIT v.0.0.3

--- Ship.py
+++ Ship.py
@@ -7,3 +7,15 @@ Velocity
 ---------------
 move()    
 """
+class Ship:
+    def __init__(self):
+        self.direction="RIGHT"
+    def updateCoordinates(self, x, y):
+            
+        if self.direction=="LEFT":
+            x=x-1
+            
+        elif self.direction=="RIGHT":
+            x=x+1
+            
+        return(x,y)
